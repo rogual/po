d57a92d2de249e7ef3966bea32f87adc80399031
Robin Allen <roblovski@gmail.com>
2013-10-22 15:59:02 +0100

    Update library paths for CEF

--- Recipes/cef.py
+++ Recipes/cef.py
@@ -1,6 +1,22 @@
+from os.path import join
+
 import simple
+import environment
+import standard
 
 url = "https://chromiumembedded.googlecode.com/" \
       "files/cef_binary_1.1180.832_windows.zip"
 
 recipe = simple.simple('cef', 'Chromium Embedded Framework', url)
+
+
+@environment.libraries.implement(recipe)
+def libraries(package):
+    loc = package[standard.a_location]
+    return [join(loc, 'lib', 'Release'), join(loc, 'Release', 'lib')]
+
+
+@environment.headers.implement(recipe)
+def include(package):
+    loc = package[standard.a_location]
+    return [loc, join(loc, 'include')]
