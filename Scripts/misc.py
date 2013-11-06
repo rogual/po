e208fe1b38b03ea878a35f77f586088ffb397d12
Robin Allen <roblovski@gmail.com>
2013-11-06 16:25:45 +0000

    Added "po which" command
    Like "po cd", but only prints the path; doesn't change to it.

--- Scripts/misc.py
+++ Scripts/misc.py
@@ -6,6 +6,7 @@ import paths
 
 from util import error
 
+
 @package.command()
 def cd(package_specifier):
     """Change to the directory of the specified package.
@@ -14,19 +15,38 @@ def cd(package_specifier):
     """
 
     if package_specifier:
-        ps = list(repository.find_installed_packages(package_specifier))
-
-        if len(ps) == 0:
-            return error("No matching packages.")
-        elif len(ps) != 1:
-            return error("Ambiguous command.")
-
-        path = ps[0].get(attributes.a_location)
-        if not path:
-            return error("Package has no associated location.")
+        path, err = get_path(package_specifier)
+        if err:
+            return err
     else:
         path = paths.site
 
     with environment.exit() as exit:
         exit.write('@cd "%s"\n' % path)
 
+
+@package.command()
+def which(package_specifier):
+    """Show where the specified package is installed."""
+
+    if package_specifier:
+        path, err = get_path(package_specifier)
+        if err:
+            return err
+    else:
+        return error("Usage: po which PACKAGE")
+
+
+def get_path(package_specifier):
+    ps = list(repository.find_installed_packages(package_specifier))
+
+    if len(ps) == 0:
+        return error("No matching packages."), None
+    elif len(ps) != 1:
+        return error("Ambiguous command."), None
+
+    path = ps[0].get(attributes.a_location)
+    if not path:
+        return error("Package has no associated location."), None
+
+    return None, path
